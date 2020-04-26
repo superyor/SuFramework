3b756effaeffebc935fe644fc8b3d47370951f7b
Superyu <49453028+superyor@users.noreply.github.com>
2020-04-26 18:13:03 +0200

    Proper release

---
+++ SuFramework [Module Version].lua
@@ -0,0 +1,327 @@
+local SuFramework = {
+    mainGuiObjects = {};
+    menuCreated = false;
+    varnamePrefix = "",
+    categoryNames = {},
+    featureNames = {},
+    categoryCount = 0;
+    featureCount = 0;
+    currentColumn = 0;
+    currentY = 0;
+    categories = {};
+}
+
+function SuFramework.createMenu(varnamePrefix, reference, tabName)
+    if not varnamePrefix then
+        return false;
+    end
+
+    SuFramework.mainGuiObjects.tab = gui.Tab(reference, varnamePrefix .. ".tab", tabName)
+    SuFramework.mainGuiObjects.selectorGroup = gui.Groupbox(SuFramework.mainGuiObjects.tab, "Selector", 16, 16, 600, 64)
+    SuFramework.mainGuiObjects.categorySelector = gui.Combobox(SuFramework.mainGuiObjects.selectorGroup, "selector.category", "Category", "")
+    SuFramework.mainGuiObjects.featureSelector = gui.Combobox(SuFramework.mainGuiObjects.selectorGroup, "selector.feature", "Feature", "")
+    SuFramework.mainGuiObjects.configureGroup = gui.Groupbox(SuFramework.mainGuiObjects.tab, "Configure", 16, 64+64, 600, 64)
+    SuFramework.mainGuiObjects.categorySelector:SetWidth(300-16-4)
+    SuFramework.mainGuiObjects.featureSelector:SetPosX(300-8)
+    SuFramework.mainGuiObjects.featureSelector:SetPosY(0)
+    SuFramework.mainGuiObjects.featureSelector:SetWidth(300-16-4)
+    SuFramework.varnamePrefix = varnamePrefix;
+    SuFramework.menuCreated = true;
+
+    return true;
+end
+
+function SuFramework.addCategory(categoryName)
+
+    if SuFramework.menuCreated == false then
+        return false;
+    end
+
+    table.insert(SuFramework.categoryNames, categoryName)
+    SuFramework.mainGuiObjects.categorySelector:SetOptions(unpack(SuFramework.categoryNames))
+    SuFramework.categoryCount = SuFramework.categoryCount + 1;
+    SuFramework.featureCount = 0;
+    SuFramework.categories[SuFramework.categoryCount] = {}
+    return true;
+end
+
+function SuFramework.addFeature(featureName)
+
+    if SuFramework.menuCreated == false then
+        return false;
+    end
+
+    SuFramework.featureCount = SuFramework.featureCount + 1;
+
+    local temp = SuFramework.featureNames[SuFramework.categoryCount];
+    if temp == nil then
+        SuFramework.featureNames[SuFramework.categoryCount] = {};
+        table.insert(SuFramework.featureNames[SuFramework.categoryCount], featureName)
+    else
+        table.insert(SuFramework.featureNames[SuFramework.categoryCount], featureName)
+    end
+
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount] = {};
+    SuFramework.currentColumn = 1;
+    SuFramework.currentY = 0;
+    return true;
+end
+
+function SuFramework.nextColumn()
+
+    if SuFramework.currentColumn + 1 < 3 then
+        SuFramework.currentColumn = SuFramework.currentColumn + 1;
+        SuFramework.currentY = 0;
+        return true;
+    else
+        return false;
+    end
+end
+
+function SuFramework.addCheckbox(varname, name, defaultValue, description, customParent)
+
+    if SuFramework.menuCreated == false then
+        return false;
+    end
+
+    local catName = SuFramework.categoryNames[SuFramework.categoryCount]
+    catName = catName:gsub(" ", "");
+    catName = catName:lower();
+
+    local featName = SuFramework.featureNames[SuFramework.categoryCount][SuFramework.featureCount];
+    featName = featName:gsub(" ", "");
+    featName = featName:lower();
+
+    local awVarname = catName .. "." .. featName .. "." .. varname:lower();
+
+    if customParent then
+        SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname] =
+        gui.Checkbox(customParent, awVarname, name, defaultValue);
+    else
+        SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname] =
+        gui.Checkbox(SuFramework.mainGuiObjects.configureGroup, awVarname, name, defaultValue);
+    end
+
+    if SuFramework.currentColumn == 2 then
+        SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetPosX(300-8);
+    end
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetPosY(SuFramework.currentY);
+
+    if description then
+        SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetDescription(description);
+        SuFramework.currentY = SuFramework.currentY + 28*1.66;
+    else
+        SuFramework.currentY = SuFramework.currentY + 28;
+    end
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetWidth(300-16-4)
+
+    return SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname];
+end
+
+function SuFramework.addSlider(varname, name, defaultValue, min, max, stepSize, description)
+
+    if SuFramework.menuCreated == false then
+        return false;
+    end
+
+    local catName = SuFramework.categoryNames[SuFramework.categoryCount]
+    catName = catName:gsub(" ", "");
+    catName = catName:lower();
+
+    local featName = SuFramework.featureNames[SuFramework.categoryCount][SuFramework.featureCount];
+    featName = featName:gsub(" ", "");
+    featName = featName:lower();
+
+    local awVarname = catName .. "." .. featName .. "." .. varname:lower();
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname] =
+    gui.Slider(SuFramework.mainGuiObjects.configureGroup, awVarname, name, defaultValue, min, max, stepSize)
+
+    if SuFramework.currentColumn == 2 then
+        SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetPosX(300-8);
+    end
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetPosY(SuFramework.currentY);
+
+    if description then
+        SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetDescription(description);
+        SuFramework.currentY = SuFramework.currentY + 48*1.2;
+    else
+        SuFramework.currentY = SuFramework.currentY + 48;
+    end
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetWidth(300-16-4)
+
+    return SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname];
+end
+
+function SuFramework.addCombobox(varname, name, optionsTable, description)
+
+    if SuFramework.menuCreated == false then
+        return false;
+    end
+
+    local catName = SuFramework.categoryNames[SuFramework.categoryCount]
+    catName = catName:gsub(" ", "");
+    catName = catName:lower();
+
+    local featName = SuFramework.featureNames[SuFramework.categoryCount][SuFramework.featureCount];
+    featName = featName:gsub(" ", "");
+    featName = featName:lower();
+
+    local awVarname = catName .. "." .. featName .. "." .. varname:lower();
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname] =
+    gui.Combobox(SuFramework.mainGuiObjects.configureGroup, awVarname, name, unpack(optionsTable))
+
+    if SuFramework.currentColumn == 2 then
+        SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetPosX(300-8);
+    end
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetPosY(SuFramework.currentY);
+
+    if description then
+        SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetDescription(description);
+        SuFramework.currentY = SuFramework.currentY + 48*1.5;
+    else
+        SuFramework.currentY = SuFramework.currentY + 48;
+    end
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetWidth(300-16-4)
+
+    return SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname];
+end
+
+function SuFramework.addColorPicker(varname, name, colorTable)
+
+    if SuFramework.menuCreated == false then
+        return false;
+    end
+
+    local catName = SuFramework.categoryNames[SuFramework.categoryCount]
+    catName = catName:gsub(" ", "");
+    catName = catName:lower();
+
+    local featName = SuFramework.featureNames[SuFramework.categoryCount][SuFramework.featureCount];
+    featName = featName:gsub(" ", "");
+    featName = featName:lower();
+
+    local awVarname = catName .. "." .. featName .. "." .. varname:lower();
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname] =
+    gui.ColorPicker(SuFramework.mainGuiObjects.configureGroup, awVarname, name, unpack(colorTable))
+
+    if SuFramework.currentColumn == 2 then
+        SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetPosX(300-8);
+    end
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetPosY(SuFramework.currentY);
+
+    SuFramework.currentY = SuFramework.currentY + 24;
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetWidth(300-16-4)
+
+    return SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname];
+end
+
+function SuFramework.addText(varname, text)
+
+    if SuFramework.menuCreated == false then
+        return false;
+    end
+
+    local catName = SuFramework.categoryNames[SuFramework.categoryCount]
+    catName = catName:gsub(" ", "");
+    catName = catName:lower();
+
+    local featName = SuFramework.featureNames[SuFramework.categoryCount][SuFramework.featureCount];
+    featName = featName:gsub(" ", "");
+    featName = featName:lower();
+
+    local awVarname = catName .. "." .. featName .. "." .. varname:lower();
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname] =
+    gui.Text(SuFramework.mainGuiObjects.configureGroup, text)
+
+    if SuFramework.currentColumn == 2 then
+        SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetPosX(300-8);
+    end
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetPosY(SuFramework.currentY);
+
+    SuFramework.currentY = SuFramework.currentY + 24;
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetWidth(300-16-4)
+
+    return SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname];
+end
+
+function SuFramework.addEditbox(varname, name, description)
+
+    if SuFramework.menuCreated == false then
+        return false;
+    end
+
+    local catName = SuFramework.categoryNames[SuFramework.categoryCount]
+    catName = catName:gsub(" ", "");
+    catName = catName:lower();
+
+    local featName = SuFramework.featureNames[SuFramework.categoryCount][SuFramework.featureCount];
+    featName = featName:gsub(" ", "");
+    featName = featName:lower();
+
+    local awVarname = catName .. "." .. featName .. "." .. varname:lower();
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname] =
+    gui.Editbox(SuFramework.mainGuiObjects.configureGroup, awVarname, name)
+
+    if SuFramework.currentColumn == 2 then
+        SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetPosX(300-8);
+    end
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetPosY(SuFramework.currentY);
+
+    if description then
+        SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetDescription(description);
+        SuFramework.currentY = SuFramework.currentY + 48*1.5;
+    else
+        SuFramework.currentY = SuFramework.currentY + 48;
+    end
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetWidth(300-16-4)
+
+    return SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname];
+end
+
+function SuFramework.addMultibox(varname, name, description)
+
+    if SuFramework.menuCreated == false then
+        return false;
+    end
+
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname] =
+    gui.Multibox(SuFramework.mainGuiObjects.configureGroup, name)
+
+    if SuFramework.currentColumn == 2 then
+        SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetPosX(300-8);
+    end
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetPosY(SuFramework.currentY);
+
+    if description then
+        SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetDescription(description);
+        SuFramework.currentY = SuFramework.currentY + 48*1.5;
+    else
+        SuFramework.currentY = SuFramework.currentY + 48;
+    end
+    SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname]:SetWidth(300-16-4)
+
+    return SuFramework.categories[SuFramework.categoryCount][SuFramework.featureCount][varname];
+end
+
+function SuFramework.handleUI()
+
+    SuFramework.mainGuiObjects.featureSelector:SetOptions(unpack(SuFramework.featureNames[SuFramework.mainGuiObjects.categorySelector:GetValue() + 1]))
+
+    for catKey, categories in pairs(SuFramework.categories) do
+        for featKey, features in pairs(categories) do
+            for guiKey, guiObject in pairs(features) do
+                if catKey == SuFramework.mainGuiObjects.categorySelector:GetValue() + 1 then
+                    if featKey == SuFramework.mainGuiObjects.featureSelector:GetValue() + 1 then
+                        guiObject:SetInvisible(false);
+                    else
+                        guiObject:SetInvisible(true);
+                    end
+                else
+                    guiObject:SetInvisible(true);
+                end
+            end
+        end
+    end
+end
+
+return SuFramework;
