781f20b2ed43bdce5f0a77419d2e2eddf4fe12c4
Superyu <49453028+superyor@users.noreply.github.com>
2020-04-26 17:39:58 +0200

    Update SuFramework.lua

--- SuFramework.lua
+++ SuFramework.lua
@@ -1,6 +1,4 @@
---- Still in development, don't use yet.
-
-local framework = {
+local SuFramework = {
     mainGuiObjects = {};
     menuCreated = false;
     varnamePrefix = "",
@@ -12,9 +10,9 @@ local framework = {
     currentY = 0;
     categories = {};
 }
-framework.__index = framework;
+SuFramework.__index = SuFramework;
 
-function framework:createMenu(varnamePrefix, reference, tabName)
+function SuFramework:createMenu(varnamePrefix, reference, tabName)
     if not varnamePrefix then
         return false;
     end
@@ -34,7 +32,7 @@ function framework:createMenu(varnamePrefix, reference, tabName)
     return true;
 end
 
-function framework:addCategory(categoryName)
+function SuFramework:addCategory(categoryName)
 
     if self.menuCreated == false then
         return false;
@@ -43,28 +41,85 @@ function framework:addCategory(categoryName)
     table.insert(self.categoryNames, categoryName)
     self.mainGuiObjects.categorySelector:SetOptions(unpack(self.categoryNames))
     self.categoryCount = self.categoryCount + 1;
+    self.featureCount = 0;
     self.categories[self.categoryCount] = {}
     return true;
 end
 
-function framework:addFeature(featureName)
+function SuFramework:addFeature(featureName)
 
     if self.menuCreated == false then
         return false;
     end
 
-    self.featureNames[self.categoryCount] = {
-        [self.featureCount] = featureName;
-    };
-
     self.featureCount = self.featureCount + 1;
+
+    local temp = self.featureNames[self.categoryCount];
+    if temp == nil then
+        self.featureNames[self.categoryCount] = {};
+        table.insert(self.featureNames[self.categoryCount], featureName)
+    else
+        table.insert(self.featureNames[self.categoryCount], featureName)
+    end
+
     self.categories[self.categoryCount][self.featureCount] = {};
     self.currentColumn = 1;
     self.currentY = 0;
     return true;
 end
 
-function framework:addCheckbox(varname, name, defaultValue, description)
+function SuFramework:nextColumn()
+
+    if self.currentColumn + 1 < 3 then
+        self.currentColumn = self.currentColumn + 1;
+        self.currentY = 0;
+        return true;
+    else
+        return false;
+    end
+end
+
+function SuFramework:addCheckbox(varname, name, defaultValue, description, customParent)
+
+    if self.menuCreated == false then
+        return false;
+    end
+
+    local catName = self.categoryNames[self.categoryCount]
+    catName = catName:gsub(" ", "");
+    catName = catName:lower();
+
+    local featName = self.featureNames[self.categoryCount][self.featureCount];
+    featName = featName:gsub(" ", "");
+    featName = featName:lower();
+
+    local awVarname = catName .. "." .. featName .. "." .. varname:lower();
+
+    if customParent then
+        self.categories[self.categoryCount][self.featureCount][varname] =
+        gui.Checkbox(customParent, awVarname, name, defaultValue);
+    else
+        self.categories[self.categoryCount][self.featureCount][varname] =
+        gui.Checkbox(self.mainGuiObjects.configureGroup, awVarname, name, defaultValue);
+    end
+
+    if self.currentColumn == 2 then
+        self.categories[self.categoryCount][self.featureCount][varname]:SetPosX(300-8);
+    end
+    self.categories[self.categoryCount][self.featureCount][varname]:SetPosY(self.currentY);
+
+    if description then
+        self.categories[self.categoryCount][self.featureCount][varname]:SetDescription(description);
+        self.currentY = self.currentY + 28*1.66;
+    else
+        self.currentY = self.currentY + 28;
+    end
+    self.categories[self.categoryCount][self.featureCount][varname]:SetWidth(300-16-4)
+
+    return self.categories[self.categoryCount][self.featureCount][varname];
+end
+
+function SuFramework:addSlider(varname, name, defaultValue, min, max, stepSize, description)
 
     if self.menuCreated == false then
         return false;
@@ -80,7 +135,7 @@ function framework:addCheckbox(varname, name, defaultValue, description)
 
     local awVarname = catName .. "." .. featName .. "." .. varname:lower();
     self.categories[self.categoryCount][self.featureCount][varname] =
-    gui.Checkbox(self.mainGuiObjects.configureGroup, awVarname, name, defaultValue);
+    gui.Slider(self.mainGuiObjects.configureGroup, awVarname, name, defaultValue, min, max, stepSize)
 
     if self.currentColumn == 2 then
         self.categories[self.categoryCount][self.featureCount][varname]:SetPosX(300-8);
@@ -89,22 +144,173 @@ function framework:addCheckbox(varname, name, defaultValue, description)
 
     if description then
         self.categories[self.categoryCount][self.featureCount][varname]:SetDescription(description);
-        self.currentY = self.currentY + 28*1.5;
+        self.currentY = self.currentY + 48*1.2;
     else
-        self.currentY = self.currentY + 28;
+        self.currentY = self.currentY + 48;
     end
     self.categories[self.categoryCount][self.featureCount][varname]:SetWidth(300-16-4)
 
     return self.categories[self.categoryCount][self.featureCount][varname];
 end
 
-function framework:handleUI()
+function SuFramework:addCombobox(varname, name, optionsTable, description)
+
+    if self.menuCreated == false then
+        return false;
+    end
+
+    local catName = self.categoryNames[self.categoryCount]
+    catName = catName:gsub(" ", "");
+    catName = catName:lower();
+
+    local featName = self.featureNames[self.categoryCount][self.featureCount];
+    featName = featName:gsub(" ", "");
+    featName = featName:lower();
+
+    local awVarname = catName .. "." .. featName .. "." .. varname:lower();
+    self.categories[self.categoryCount][self.featureCount][varname] =
+    gui.Combobox(self.mainGuiObjects.configureGroup, awVarname, name, unpack(optionsTable))
+
+    if self.currentColumn == 2 then
+        self.categories[self.categoryCount][self.featureCount][varname]:SetPosX(300-8);
+    end
+    self.categories[self.categoryCount][self.featureCount][varname]:SetPosY(self.currentY);
+
+    if description then
+        self.categories[self.categoryCount][self.featureCount][varname]:SetDescription(description);
+        self.currentY = self.currentY + 48*1.5;
+    else
+        self.currentY = self.currentY + 48;
+    end
+    self.categories[self.categoryCount][self.featureCount][varname]:SetWidth(300-16-4)
+
+    return self.categories[self.categoryCount][self.featureCount][varname];
+end
+
+function SuFramework:addColorPicker(varname, name, colorTable)
+
+    if self.menuCreated == false then
+        return false;
+    end
+
+    local catName = self.categoryNames[self.categoryCount]
+    catName = catName:gsub(" ", "");
+    catName = catName:lower();
+
+    local featName = self.featureNames[self.categoryCount][self.featureCount];
+    featName = featName:gsub(" ", "");
+    featName = featName:lower();
+
+    local awVarname = catName .. "." .. featName .. "." .. varname:lower();
+    self.categories[self.categoryCount][self.featureCount][varname] =
+    gui.ColorPicker(self.mainGuiObjects.configureGroup, awVarname, name, unpack(colorTable))
+
+    if self.currentColumn == 2 then
+        self.categories[self.categoryCount][self.featureCount][varname]:SetPosX(300-8);
+    end
+    self.categories[self.categoryCount][self.featureCount][varname]:SetPosY(self.currentY);
+
+    self.currentY = self.currentY + 24;
+    self.categories[self.categoryCount][self.featureCount][varname]:SetWidth(300-16-4)
+
+    return self.categories[self.categoryCount][self.featureCount][varname];
+end
+
+function SuFramework:addText(varname, text)
+
+    if self.menuCreated == false then
+        return false;
+    end
+
+    local catName = self.categoryNames[self.categoryCount]
+    catName = catName:gsub(" ", "");
+    catName = catName:lower();
+
+    local featName = self.featureNames[self.categoryCount][self.featureCount];
+    featName = featName:gsub(" ", "");
+    featName = featName:lower();
+
+    local awVarname = catName .. "." .. featName .. "." .. varname:lower();
+    self.categories[self.categoryCount][self.featureCount][varname] =
+    gui.Text(self.mainGuiObjects.configureGroup, text)
+
+    if self.currentColumn == 2 then
+        self.categories[self.categoryCount][self.featureCount][varname]:SetPosX(300-8);
+    end
+    self.categories[self.categoryCount][self.featureCount][varname]:SetPosY(self.currentY);
+
+    self.currentY = self.currentY + 24;
+    self.categories[self.categoryCount][self.featureCount][varname]:SetWidth(300-16-4)
+
+    return self.categories[self.categoryCount][self.featureCount][varname];
+end
+
+function SuFramework:addEditbox(varname, name, description)
+
+    if self.menuCreated == false then
+        return false;
+    end
+
+    local catName = self.categoryNames[self.categoryCount]
+    catName = catName:gsub(" ", "");
+    catName = catName:lower();
+
+    local featName = self.featureNames[self.categoryCount][self.featureCount];
+    featName = featName:gsub(" ", "");
+    featName = featName:lower();
+
+    local awVarname = catName .. "." .. featName .. "." .. varname:lower();
+    self.categories[self.categoryCount][self.featureCount][varname] =
+    gui.Editbox(self.mainGuiObjects.configureGroup, awVarname, name)
+
+    if self.currentColumn == 2 then
+        self.categories[self.categoryCount][self.featureCount][varname]:SetPosX(300-8);
+    end
+    self.categories[self.categoryCount][self.featureCount][varname]:SetPosY(self.currentY);
+
+    if description then
+        self.categories[self.categoryCount][self.featureCount][varname]:SetDescription(description);
+        self.currentY = self.currentY + 48*1.5;
+    else
+        self.currentY = self.currentY + 48;
+    end
+    self.categories[self.categoryCount][self.featureCount][varname]:SetWidth(300-16-4)
+
+    return self.categories[self.categoryCount][self.featureCount][varname];
+end
+
+function SuFramework:addMultibox(varname, name, description)
+
+    if self.menuCreated == false then
+        return false;
+    end
+
+    self.categories[self.categoryCount][self.featureCount][varname] =
+    gui.Multibox(self.mainGuiObjects.configureGroup, name)
+
+    if self.currentColumn == 2 then
+        self.categories[self.categoryCount][self.featureCount][varname]:SetPosX(300-8);
+    end
+    self.categories[self.categoryCount][self.featureCount][varname]:SetPosY(self.currentY);
+
+    if description then
+        self.categories[self.categoryCount][self.featureCount][varname]:SetDescription(description);
+        self.currentY = self.currentY + 48*1.5;
+    else
+        self.currentY = self.currentY + 48;
+    end
+    self.categories[self.categoryCount][self.featureCount][varname]:SetWidth(300-16-4)
+
+    return self.categories[self.categoryCount][self.featureCount][varname];
+end
+
+function SuFramework:handleUI()
+
+    self.mainGuiObjects.featureSelector:SetOptions(unpack(self.featureNames[self.mainGuiObjects.categorySelector:GetValue() + 1]))
+
     for catKey, categories in pairs(self.categories) do
         for featKey, features in pairs(categories) do
             for guiKey, guiObject in pairs(features) do
-
-                self.mainGuiObjects.featureSelector:SetOptions(unpack(self.featureNames[self.categoryCount][self.featureCount]))
-
                 if catKey == self.mainGuiObjects.categorySelector:GetValue() + 1 then
                     if featKey == self.mainGuiObjects.featureSelector:GetValue() + 1 then
                         guiObject:SetInvisible(false);
@@ -118,35 +324,3 @@ function framework:handleUI()
         end
     end
 end
-
-if framework:createMenu("ragesu", gui.Reference("Ragebot"), "RageSU") then
-    if framework:addCategory("Test Category") then
-        if framework:addFeature("Test Feature") then
-            local kek = framework:addCheckbox("testCheck", "Test Checkbox", true, "This is a test checkbox.")
-            local kek2 = framework:addCheckbox("testCheck2", "Test Checkbox 2", false, "This is a second test checkbox.")
-        end
-
-        if framework:addFeature("Test Feature Two") then
-            local kek3 = framework:addCheckbox("testCheck3", "Test Checkbox 3", true, "This is a third test checkbox.")
-            local kek4 = framework:addCheckbox("testCheck4", "Test Checkbox 4", true, "This is a fourth test checkbox.")
-        end
-    end
-
-    if framework:addCategory("Test Category Two") then
-        if framework:addFeature("Test Feature") then
-            local kek = framework:addCheckbox("testCheck", "Test Checkbox from Test Category 2", true, "This is a test checkbox.")
-            local kek2 = framework:addCheckbox("testCheck2", "Test Checkbox 2 from Test Category 2", false, "This is a second test checkbox.")
-        end
-
-        if framework:addFeature("Test Feature Two") then
-            local kek3 = framework:addCheckbox("testCheck3", "Test Checkbox 3 from Test Category 2", true, "This is a third test checkbox.")
-            local kek4 = framework:addCheckbox("testCheck4", "Test Checkbox 4 from Test Category 2", true, "This is a fourth test checkbox.")
-        end
-    end
-end
-
-local function hkDraw()
-    framework:handleUI()
-end
-
-callbacks.Register("Draw", hkDraw);
